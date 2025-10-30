b05cb3d3afd0305edc5815f2052f2ea10fea82be
Joel Bobeck <joel.bobeck@gmail.com>
2025-10-30 00:23:25 -0400

    chore: track only themes/awi-revamped; ignore plugins and acf-json

--- themes/awi-old/functions/custom.php
+++
@@ -1,5 +0,0 @@
-<?php
-/**
- * Custom Functions
- * Theme-specific or non-boilerplate plugin-specific functions can be added here
- */
--- themes/awi-old/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo esc_url(home_url('/')); ?>">
-	<input type="text" value="" name="s" id="s">
-	<input type="submit" id="searchsubmit" value="Search">
-</form>
